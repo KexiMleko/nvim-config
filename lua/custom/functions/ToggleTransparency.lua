96ad0392866f91fef81b4b710f6bc5c60e308b9e
Aleksa <ocokolja.aleksa@gmail.com>
2025-05-08 10:46:08 +0200

    Transparency non current window fix, telescope border fix

--- lua/custom/functions/ToggleTransparency.lua
+++ lua/custom/functions/ToggleTransparency.lua
@@ -6,6 +6,11 @@ function _G.ToggleTransparency()
   if TRANSPARENCY_ENABLED then
     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopeBorder', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { bg = 'none' })
     print 'Transparency enabled'
   else
     -- Reapply colorscheme to reset background
