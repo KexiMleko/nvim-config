7b548237aca6bca1a619177fb03641487757a5ac
Aleksa <ocokolja.aleksa@gmail.com>
2025-05-16 08:43:09 +0200

    harpoon config for telescope

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -15,6 +15,28 @@ return {
       },
     }
     -- REQUIRED
+    local conf = require('telescope.config').values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require('telescope.pickers')
+        .new({}, {
+          prompt_title = 'Harpoon',
+          finder = require('telescope.finders').new_table {
+            results = file_paths,
+          },
+          previewer = conf.file_previewer {},
+          sorter = conf.generic_sorter {},
+        })
+        :find()
+    end
+
+    vim.keymap.set('n', '<C-f>', function()
+      toggle_telescope(harpoon:list())
+    end, { desc = 'Open harpoon window' })
 
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
@@ -22,7 +44,7 @@ return {
     vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
-
+    --
     vim.keymap.set('n', '<C-h>', function()
       harpoon:list():select(1)
     end)
