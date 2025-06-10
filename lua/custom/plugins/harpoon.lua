15241581d170226fc38b611c2982102c50d64311
Keximleko <ocokolja.aleksa@gmail.com>
2025-06-10 22:35:41 +0200

    harpoon keybind change

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -54,7 +54,7 @@ return {
     vim.keymap.set('n', '<C-k>', function()
       harpoon:list():select(3)
     end)
-    vim.keymap.set('n', '<C-;>', function()
+    vim.keymap.set('n', '<C-l>', function()
       harpoon:list():select(4)
     end)
 
