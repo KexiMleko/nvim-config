3dbb743c73c0ac8863345649842100788ecb5b1c
Aleksa <ocokolja.aleksa@gmail.com>
2025-08-11 10:20:15 +0200

    perf(plugins): Lazy loading plugins telescope, treesitter, whichkey, hardtime, harpoon, todo comments.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,10 +2,20 @@ return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    { '<C-f>', desc = 'Open harpoon window' },
+    { '<leader>a', desc = 'Add to harpoon' },
+    { '<C-e>', desc = 'Toggle harpoon menu' },
+    { '<C-h>', desc = 'Harpoon select 1' },
+    { '<C-j>', desc = 'Harpoon select 2' },
+    { '<C-k>', desc = 'Harpoon select 3' },
+    { '<C-l>', desc = 'Harpoon select 4' },
+    { '<C-S-P>', desc = 'Harpoon prev' },
+    { '<C-S-N>', desc = 'Harpoon next' },
+  },
   config = function()
     local harpoon = require 'harpoon'
 
-    -- REQUIRED
     harpoon:setup {
       default = {
         display = function(list_item)
@@ -14,7 +24,7 @@ return {
         end,
       },
     }
-    -- REQUIRED
+
     local conf = require('telescope.config').values
     local function toggle_telescope(harpoon_files)
       local file_paths = {}
@@ -41,29 +51,33 @@ return {
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
     end, { desc = 'Add to harpoon' })
+
     vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
-    --
+    end, { desc = 'Toggle harpoon menu' })
+
     vim.keymap.set('n', '<C-h>', function()
       harpoon:list():select(1)
-    end)
+    end, { desc = 'Harpoon select 1' })
+
     vim.keymap.set('n', '<C-j>', function()
       harpoon:list():select(2)
-    end)
+    end, { desc = 'Harpoon select 2' })
+
     vim.keymap.set('n', '<C-k>', function()
       harpoon:list():select(3)
-    end)
+    end, { desc = 'Harpoon select 3' })
+
     vim.keymap.set('n', '<C-l>', function()
       harpoon:list():select(4)
-    end)
+    end, { desc = 'Harpoon select 4' })
 
-    -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set('n', '<C-S-P>', function()
       harpoon:list():prev()
-    end)
+    end, { desc = 'Harpoon prev' })
+
     vim.keymap.set('n', '<C-S-N>', function()
       harpoon:list():next()
-    end)
+    end, { desc = 'Harpoon next' })
   end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,39 +1,57 @@
-return { -- Fuzzy Finder (files, lsp, etc)
+return {
+  -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
-  event = 'VimEnter',
+  cmd = 'Telescope',
+  keys = {
+    { '<leader>sh', '<cmd>Telescope help_tags<cr>', desc = '[S]earch [H]elp' },
+    { '<leader>sk', '<cmd>Telescope keymaps<cr>', desc = '[S]earch [K]eymaps' },
+    { '<leader>sf', '<cmd>Telescope find_files<cr>', desc = '[S]earch [F]iles' },
+    { '<leader>ss', '<cmd>Telescope builtin<cr>', desc = '[S]earch [S]elect Telescope' },
+    { '<C-p>', '<cmd>Telescope git_files<cr>', desc = 'git files search' },
+    { '<leader>sw', '<cmd>Telescope grep_string<cr>', desc = '[S]earch current [W]ord' },
+    { '<leader>sg', '<cmd>Telescope live_grep<cr>', desc = '[S]earch by [G]rep' },
+    { '<leader>sd', '<cmd>Telescope diagnostics<cr>', desc = '[S]earch [D]iagnostics' },
+    { '<leader>sr', '<cmd>Telescope resume<cr>', desc = '[S]earch [R]esume' },
+    { '<leader>s.', '<cmd>Telescope oldfiles<cr>', desc = '[S]earch Recent Files ("." for repeat)' },
+    { '<leader><leader>', '<cmd>Telescope buffers<cr>', desc = '[ ] Find existing buffers' },
+    { '<leader>sp', '<cmd>Telescope project<cr>', desc = '[S]earch [P]rojects' },
+    {
+      '<leader>/',
+      function()
+        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = false })
+      end,
+      desc = '[/] Fuzzily search in current buffer',
+    },
+    {
+      '<leader>s/',
+      function()
+        require('telescope.builtin').live_grep { grep_open_files = true, prompt_title = 'Live Grep in Open Files' }
+      end,
+      desc = '[S]earch [/] in Open Files',
+    },
+    { '<leader>sn', '<cmd>Telescope find_files cwd=' .. vim.fn.stdpath 'config' .. '<cr>', desc = '[S]earch [N]eovim files' },
+  },
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-project.nvim',
-    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+    {
       'nvim-telescope/telescope-fzf-native.nvim',
-
-      -- `build` is used to run some command when the plugin is installed/updated.
-      -- This is only run then, not every time Neovim starts up.
       build = 'make',
-
-      -- `cond` is a condition used to determine whether this plugin should be
-      -- installed and loaded.
       cond = function()
         return vim.fn.executable 'make' == 1
       end,
     },
     { 'nvim-telescope/telescope-ui-select.nvim' },
-
-    -- Useful for getting pretty icons, but requires a Nerd Font.
     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
   config = function()
     require('telescope').setup {
-      -- You can put your default mappings / updates / etc. in here
-      --  All the info you're looking for is in `:help telescope.setup()`
-      --
       defaults = {
         path_display = { 'smart' },
         mappings = {
           i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         },
       },
-      -- pickers = {}
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
@@ -52,46 +70,8 @@ return { -- Fuzzy Finder (files, lsp, etc)
       },
     }
 
-    -- Enable Telescope extensions if they are installed
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
     pcall(require('telescope').load_extension, 'project')
-    -- See `:help telescope.builtin`
-    local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'git files search' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>sp', '<cmd>Telescope project<CR>', { desc = '[S]earch [P]rojects' })
-
-    -- Slightly advanced example of overriding default behavior and theme
-    vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end, { desc = '[/] Fuzzily search in current buffer' })
-
-    -- It's also possible to pass additional configuration options.
-    --  See `:help telescope.builtin.live_grep()` for information about particular keys
-    vim.keymap.set('n', '<leader>s/', function()
-      builtin.live_grep {
-        grep_open_files = true,
-        prompt_title = 'Live Grep in Open Files',
-      }
-    end, { desc = '[S]earch [/] in Open Files' })
-
-    -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set('n', '<leader>sn', function()
-      builtin.find_files { cwd = vim.fn.stdpath 'config' }
-    end, { desc = '[S]earch [N]eovim files' })
   end,
 }
